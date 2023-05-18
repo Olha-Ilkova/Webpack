57540ceba7fcc32cdc18e5e46ca70671bc0996ff
Olha-Ilkova <olhailkova01@gmail.com>
2023-05-18 17:11:56 +0300

    add packages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import './styles/style.css'
 import userAvatar from './assets/logo.png'
 import './styles/less/styles.less'
 import './styles/scss/styles.sass'
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import { createPopper } from '@popperjs/core';
+import postcss from 'postcss';
+import * as csstree from 'css-tree';
+import color from 'color';
+
 
 const comment = new Comment('Lesson about Webpack!!!', userAvatar);
 
